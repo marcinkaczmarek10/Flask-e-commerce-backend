7fcf845f2d76f31a0a17edf001e2e4fd8f4eed84
Marcin Kaczmarek <marcin.kacz10@gmail.com>
2022-02-03 21:39:50 +0100

    working on unittests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -8,6 +8,11 @@ class AppTestCase(unittest.TestCase):
         self.app = create_app(TestingConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.client = self.app.test_client()
 
     def tearDown(self) -> None:
         self.app_context.pop()
+
+    def test_add_to_cart(self):
+        res = self.client.post('/add-item/1')
+        self.assertEqual(res.status_code, 200)
